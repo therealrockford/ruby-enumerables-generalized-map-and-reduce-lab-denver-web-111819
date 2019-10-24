30f88b626c8b51fa678381f1196ef3d96b62a15f
Rocky Pitto <rockypitto@gmail.com>
2019-10-24 16:44:09 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,39 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  i = 0 
+  while i < array.length
+    new_array.push(yield(array[i]))
+    i += 1 
+  end
+  new_array 
+end
+
+map([1, 2, 3, -9]) do |n| n * -1 
+end 
+
+# map([1, 2, 3, -9]) { |n| n * -1}
+
+def reduce(array, sv=nil)
+  if sv 
+    sum = sv 
+    i = 0 
+  else 
+    sum = array[0]
+    i = 1 
+  end 
+  while i < array.length
+    sum = yield(sum, array[i])
+    i += 1 
+  end
+  sum
+end 
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
